6fc2e8b3419bf8e87e4ab7b83f1029f4cb3a6d3c
WekshionQuadro <wekshion@gmail.com>
2026-05-06 01:17:23 +0300

    Version 1

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// Функция, которая вызывается по обработчику событий onclick
+
 function insertToken() {
-  // Метод prompt () для запроса данных
+
   let userToken = prompt("Шаг 1: Введите токен, который вам выдал BotFather:");
 
-  // Проверка, что пользователь ввел данные, а не нажал "Отмена"
   if (userToken != null && userToken.trim() !== "") {
 
-    // Находим элемент с кодом на странице
+
     let codeBlock = document.getElementById("bot-code");
 
-    // Заменяем заглушку на введенный токен
+
     let newCode = codeBlock.innerText.replace("YOUR_TOKEN_HERE", userToken);
     codeBlock.innerText = newCode;
 
-    // Метод alert () для вывода результата
     alert("Успех! Ваш токен успешно добавлен. Теперь скопируйте код и запустите скрипт.");
   } else {
     alert("Действие отменено или токен не был введен.");
